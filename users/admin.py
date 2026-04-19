e837d9db0ff93fb5b5fbec67a3ea3742a1ba422a
UnicDuck <unicduck@gmail.com>
2026-04-19 15:58:01 +0500

    fix: resolve mypy issues in custom user admin

--- users/admin.py
+++ users/admin.py
@@ -6,8 +6,10 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (("Role", {"fields": ("role",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + (("Role", {"fields": ("role",)}),)
+    fieldsets = tuple(UserAdmin.fieldsets or ()) + (("Role", {"fields": ("role",)}),)
+    add_fieldsets = tuple(UserAdmin.add_fieldsets or ()) + (
+        ("Role", {"fields": ("role",)}),
+    )
     list_display = ("username", "email", "role", "is_staff", "is_superuser")
     list_filter = ("role", "is_staff", "is_superuser")
     search_fields = ("email", "username")
